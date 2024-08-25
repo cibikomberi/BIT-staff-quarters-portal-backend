bed92ff43df1a5a41d838fe99efe8e85a2ad758a
= <=>
2024-08-25 17:41:46 +0530

    empty project

---
+++ src/main/java/com/bit/quarters/QuartersApplication.java
@@ -0,0 +1,13 @@
+package com.bit.quarters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartersApplication.class, args);
+	}
+
+}
